edff8b6ef7e71c794c9b1f5b7f511f06be37c971
noa klein <noak099@gmail.com>
2025-06-16 15:36:56 +0300

    Added excercise 3.2.5

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,5 @@
+import underAgeException
+########################## 3.1.3 ##########################
 def stop_iteration_err() -> None:
     """
     raises StopIteration error
@@ -35,18 +37,18 @@ def key_err() -> None:
     dict["f"]
 
 
-def sytax_err() -> None:
-    """
-    raises SyntaxError error
-    """
-    l = {"g": "g" "h": "g"}
+# def sytax_err() -> None:
+#     """
+#     raises SyntaxError error
+#     """
+#     l = {"g": "g" "h": "g"}
 
 
-def indentation_err() -> None:
-    """
-    raises IndentationError error
-    """
-print("xxx")
+# def indentation_err() -> None:
+#     """
+#     raises IndentationError error
+#     """
+# print("xxx")
 
 
 def type_err() -> None:
@@ -54,3 +56,38 @@ def type_err() -> None:
     raises TypeError error
     """
     l = 'w' + 4
+
+########################## 3.2.5 ##########################
+
+
+def read_file(file_name: str) -> str:
+    """
+    open and reads file
+    :param file_name: path to file
+    :return: files content
+    """
+    string: str = "__CONTENT_START__\n"
+    try:
+        file = open(file_name, 'r')
+        string += file.read() + "\n"
+        file.close()
+    except FileNotFoundError:
+        string += "__NO_SUCH_FILE__\n"
+    finally:
+        string += "__CONTENT_END__\n"
+        return string
+
+
+########################## 3.2.5 ##########################
+
+def send_invitation(name: str, age: int) -> None:
+    """
+    sends an invitation to all adults
+    :param name: invitee's name
+    :param age: invitee's age
+    :return: None, raises exception if they're underage
+    """
+    if int(age) < 18:
+        raise underAgeException.UnderAge(age)
+    else:
+        print("You should send an invite to " + name)
