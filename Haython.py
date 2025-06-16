7248555cb2e0550858b97ecfec0a5fb8b6cbe8b3
noa klein <noak099@gmail.com>
2025-06-16 09:57:24 +0300

    Added type hints

--- Haython.py
+++ Haython.py
@@ -2,30 +2,30 @@ class Animal:
     """
     A class representing an animal
     """
-    zoo_name = "Hayaton"
+    zoo_name: str = "Hayaton"
 
-    def __init__(self, name, hunger=0):
+    def __init__(self, name: str, hunger: int = 0) -> None:
         """
         init function
         :param name: animals name
         :param hunger: animals hunger level
         """
-        self._name = name
-        self._hunger = hunger
+        self._name: str = name
+        self._hunger: int = hunger
 
-    def get_name(self):
+    def get_name(self) -> str:
         """
         :return: animals name
         """
         return self._name
 
-    def is_hungry(self):
+    def is_hungry(self) -> bool:
         """
         :return: true if animal is hungry, false otherwise
         """
         return self._hunger > 0
 
-    def feed(self):
+    def feed(self) -> None:
         """
         feeds animal - lowers hunger level
         :return: None
@@ -33,7 +33,7 @@ class Animal:
         if self._hunger > 0:
             self._hunger -= 1
 
-    def talk(self):
+    def talk(self) -> None:
         """
         prints animals phrase - needs to be defined in inheriting classes
         :return: None
@@ -45,14 +45,14 @@ class Dog(Animal):
     """
     A class representing a dog
     """
-    def talk(self):
+    def talk(self) -> None:
         """
         prints dogs phrase
         :return: None
         """
         print("woof woof")
 
-    def fetch_stick(self):
+    def fetch_stick(self) -> None:
         """
         fetches stick
         :return: None
@@ -64,14 +64,14 @@ class Cat(Animal):
     """
     A class representing a cat
     """
-    def talk(self):
+    def talk(self) -> None:
         """
         prints cats phrase
         :return: None
         """
         print("meow")
 
-    def chase_laser(self):
+    def chase_laser(self) -> None:
         """"
         chases laser
         :return: None
@@ -83,7 +83,7 @@ class Skunk(Animal):
     """
     A class representing a skunk
     """
-    def __init__(self, name, hunger, stink_count=6):
+    def __init__(self, name: str, hunger: int, stink_count: int = 6) -> None:
         """
         init function
         :param name: skunks name
@@ -91,16 +91,16 @@ class Skunk(Animal):
         :param stink_count: skunks stick count
         """
         Animal.__init__(self, name, hunger)
-        self._stink_count = stink_count
+        self._stink_count: int = stink_count
 
-    def talk(self):
+    def talk(self) -> None:
         """
         prints skunks phrase
         :return: None
         """
         print("tsssss")
 
-    def stink(self):
+    def stink(self) -> None:
         """
         stinks
         :return: None
@@ -112,14 +112,14 @@ class Unicorn(Animal):
     """
     A class representing a unicorn
     """
-    def talk(self):
+    def talk(self) -> None:
         """
         prints unicorn phrase
         :return: None
         """
         print("Good day, darling")
 
-    def sing(self):
+    def sing(self) -> None:
         """
         makes unicorn sing
         :return: None
@@ -131,7 +131,7 @@ class Dragon(Animal):
     """
     A class representing a dragon
     """
-    def __init__(self, name, hunger, color="Green"):
+    def __init__(self, name: str, hunger: int, color: str = "Green") -> None:
         """
         init function
         :param name: dragons name
@@ -139,16 +139,16 @@ class Dragon(Animal):
         :param color: dragons color
         """
         Animal.__init__(self, name, hunger)
-        self._color = color
+        self._color: str = color
 
-    def talk(self):
+    def talk(self) -> None:
         """
         prints dragons phrase
         :return: None
         """
         print("Raaaawr")
 
-    def breath_fire(self):
+    def breath_fire(self) -> None:
         """
         makes dragon breath fire
         :return: None
--- oop.py
+++ oop.py
@@ -2,31 +2,31 @@ class Ferret:
     """
     A class representing a ferret
     """
-    count_animals = 0
+    count_animals: int = 0
 
-    def __init__(self, name="Sam"):
+    def __init__(self, name: str = "Sam") -> None:
         """
         init function
         :param name: ferrets name
         """
-        self._name = name
-        self._age = 0
+        self._name: str = name
+        self._age: int = 0
         Ferret.count_animals += 1
 
-    def birthday(self):
+    def birthday(self) -> None:
         """
         increases ferrets age by one
         :return: None
         """
         self._age += 1
 
-    def get_age(self):
+    def get_age(self) -> int:
         """
         :return: ferrets age
         """
         return self.age
 
-    def set_name(self, name):
+    def set_name(self, name: str) -> None:
         """
         changes ferrets name
         :param name: the new name
@@ -34,7 +34,7 @@ class Ferret:
         """
         self._name = name
 
-    def get_name(self):
+    def get_name(self) -> str:
         """
         :return: the ferrets name
         """
@@ -45,7 +45,7 @@ class Pixel:
     """
     A class representing a pixel
     """
-    def __init__(self, x=0, y=0, red=0, green=0, blue=0):
+    def __init__(self, x: int = 0, y: int = 0, red: int = 0, green: int = 0, blue: int = 0) -> None:
         """
         init functions
         :param x: x coordinate
@@ -54,13 +54,13 @@ class Pixel:
         :param green: a value between 0 and 255
         :param blue: a value between 0 and 255
         """
-        self._x = x
-        self._y = y
-        self._red = red
-        self._green = green
-        self._blue = blue
+        self._x: int = x
+        self._y: int = y
+        self._red: int = red
+        self._green: int = green
+        self._blue: int = blue
 
-    def set_coords(self, x, y):
+    def set_coords(self, x, y) -> None:
         """
         sets pixel's new coordinate
         :param x: new x coordinate
@@ -70,22 +70,22 @@ class Pixel:
         self._x = x
         self._y = y
 
-    def set_grayscale(self):
+    def set_grayscale(self) -> None:
         """
         Changes pixel's color to grayscale
         :return: None
         """
-        avg = round((self._red + self._green + self._blue) / 3)
+        avg: int = round((self._red + self._green + self._blue) / 3)
         self._red = avg
         self._blue = avg
         self._green = avg
 
-    def print_pixel_info(self):
+    def print_pixel_info(self) -> None:
         """
         prints pixel's values
         :return: None
         """
-        string = f"X: {self._x}, Y: {self._y}, Color: ({self._red}, {self._green}, {self._blue})"
+        string: str = f"X: {self._x}, Y: {self._y}, Color: ({self._red}, {self._green}, {self._blue})"
         if [self._blue, self._red, self._green].count(0) == 2:
             if self._blue > 0:
                 string += " Blue\n"
@@ -97,10 +97,10 @@ class Pixel:
 
 
 class BigThing:
-    def __init__(self, thing):
+    def __init__(self, thing) -> None:
         self._thing = thing
 
-    def size(self):
+    def size(self) -> int:
         if type(self._thing) == int:
             return int
         return len(self._thing)
@@ -110,16 +110,16 @@ class BigCat(BigThing):
     """
     A class representing a cat
     """
-    def __init__(self, thing, weight):
+    def __init__(self, thing, weight) -> None:
         """
         init function
         :param thing: cats name
         :param weight: cats weight
         """
         BigThing.__init__(self, thing)
-        self._weight = weight
+        self._weight: int = weight
 
-    def size(self):
+    def size(self) -> str:
         """
         :return: a string representing the cats size
         """
