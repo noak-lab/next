b79be90829b7c76f7eeb7c9321a57e9e29131727
noa klein <noak099@gmail.com>
2025-06-17 14:28:06 +0300

    Added documentation

--- unit4/finalExercise.py
+++ unit4/finalExercise.py
@@ -1,33 +1,60 @@
-def gen_secs():
-    sec = 0
-    while sec < 60:
+from typing import Generator
+
+SEC_IN_MIN: int = 60
+MIN_IN_HOUR: int = 60
+HOUR_IN_DAY: int = 24
+FEB_IN_LEAP_YEAR: int = 29
+MONTHS_IN_YEAR: int = 12
+FEB: int = 2
+DAYS_IN_MONTH: list[int] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def gen_secs() -> Generator:
+    """
+    Generates seconds in a minute
+    :return: a generator containing the second
+    """
+    sec: int = 0
+    while sec < SEC_IN_MIN:
         yield sec
         sec += 1
 
 
-def gen_minutes():
-    mnts = 0
-    while mnts < 60:
+def gen_minutes() -> Generator:
+    """
+    Generates minutes in an hour
+    :return: a generator containing the minute
+    """
+    mnts: int = 0
+    while mnts < MIN_IN_HOUR:
         yield mnts
         mnts += 1
 
 
-def gen_hours():
-    hour = 0
-    while hour < 24:
+def gen_hours() -> Generator:
+    """
+    Generates hours in a day
+    :return: a generator containing the hour
+    """
+    hour: int = 0
+    while hour < HOUR_IN_DAY:
         yield hour
         hour += 1
 
 
-def gen_time():
-    end_of_clock = False
-    sec_gen = gen_secs()
-    mnts_gen = gen_minutes()
-    hours_gen = gen_hours()
+def gen_time() -> Generator:
+    """
+    Generates the time in a day
+    :return: a generator containing a string that represents the time
+    """
+    end_of_clock: bool = False
+    sec_gen: Generator[int] = gen_secs()
+    mnts_gen: Generator[int] = gen_minutes()
+    hours_gen: Generator[int] = gen_hours()
 
-    sec = sec_gen.__next__()
-    mnts = mnts_gen.__next__()
-    hour = hours_gen.__next__()
+    sec: int = sec_gen.__next__()
+    mnts: int = mnts_gen.__next__()
+    hour: int = hours_gen.__next__()
 
     yield f"{hour}:{mnts}:{sec}"
     while not end_of_clock:
@@ -47,32 +74,51 @@ def gen_time():
             yield f"{hour}:{mnts}:{sec}"
 
 
-def gen_years(start=2019):
-    year = start
+def gen_years(start: int = 2019) -> Generator:
+    """
+    Generates a year
+    :param start: the year the generator starts from
+    :return: a generator containing the year
+    """
+    year: int = start
     while True:
         yield year
         year += 1
 
 
-def gen_months():
-    month = 1
-    while month < 13:
-        yield month
+def gen_months() -> Generator:
+    """
+    Generates month in a year
+    :return: a generator containing the month
+    """
+    month: int = 0
+    while month < MONTHS_IN_YEAR:
         month += 1
+        yield month
 
 
-def gen_days(month, leap_year=True):
-    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    day = 1
-    days = months[month]
-    if month == 2 and leap_year:
-        days = 29
+def gen_days(month: int, leap_year: bool = True) -> Generator:
+    """
+    Generates day in a month
+    :param month: the month
+    :param leap_year: whether it's a leap year or not
+    :return: a generator containing the day
+    """
+    day: int = 1
+    days: int = DAYS_IN_MONTH[month - 1]
+    if month == FEB and leap_year:
+        days = FEB_IN_LEAP_YEAR
     while day < days:
         yield day
         day += 1
 
 
-def is_leap_year(year: int):
+def is_leap_year(year: int) -> bool:
+    """
+    Checks whether given year is a leap year or not
+    :param year: the year
+    :return: True if it's a leap year, false otherwise
+    """
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
@@ -82,16 +128,20 @@ def is_leap_year(year: int):
     return False
 
 
-def gen_date():
-    year_gen = gen_years()
-    month_gen = gen_months()
-    time_gen = gen_time()
+def gen_date() -> Generator:
+    """
+    Generates the date
+    :return: a generator containing a string that represents the time and date
+    """
+    year_gen: Generator[int] = gen_years()
+    month_gen: Generator[int] = gen_months()
+    time_gen: Generator[str] = gen_time()
 
-    year = year_gen.__next__()
-    month = month_gen.__next__()
+    year: int = year_gen.__next__()
+    month: int = month_gen.__next__()
     day_gen = gen_days(month, is_leap_year(year))
-    day = day_gen.__next__()
-    time = time_gen.__next__()
+    day: int = day_gen.__next__()
+    time: str = time_gen.__next__()
 
     yield f"{day}/{month}/{year} " + time
     while True:
@@ -124,8 +174,8 @@ def main():
     except RuntimeError:
         pass
 
-    x = 1
-    gen = gen_date()
+    x: int = 1
+    gen: Generator[str] = gen_date()
     while True:
         if x % 1000000 == 0:
             print(gen.__next__())
@@ -134,3 +184,7 @@ def main():
             gen.__next__()
         except StopIteration:
             pass
+
+
+if __name__ == '__main__':
+    main()
--- unit4/generators.py
+++ unit4/generators.py
@@ -1,16 +1,27 @@
-################################## 4.1.2 ##################################
+from typing import Generator
 import itertools
+################################## 4.1.2 ##################################
 
 
 def translate(sentence: str) -> str:
-    words = {'esta': 'is', 'la': 'the', 'en': 'in', 'gato': 'cat', 'casa': 'house', 'el': 'the'}
-    gen = (words[i] for i in sentence.split(' '))
+    """
+    translates a sentence from spanish to english
+    :param sentence: original sentence
+    :return: the translated sentence
+    """
+    words: dict[str] = {'esta': 'is', 'la': 'the', 'en': 'in', 'gato': 'cat', 'casa': 'house', 'el': 'the'}
+    gen: Generator[str] = (words[i] for i in sentence.split(' '))
     return ' '.join(list(gen))
 
 ################################## 4.1.3 ##################################
 
 
-def is_prime(n):
+def is_prime(n: int) -> bool:
+    """
+    checks if n is a prime number
+    :param n: the number
+    :return: True if prime, false otherwise
+    """
     # Corner case
     if n <= 1:
         return False
@@ -22,26 +33,36 @@ def is_prime(n):
 
 
 def first_prime_over(n: int) -> int:
-    gen = (i for i in range(n + 1, n*2 + 1) if is_prime(i))
+    """
+    returns the first prime number that's bigger than n
+    :param n: base number
+    :return: the prime number
+    """
+    gen: Generator[int] = (i for i in range(n + 1, n*2 + 1) if is_prime(i))
     return gen.__next__()
 
 
 ################################## 4.2.2 ##################################
 
 
-def parse_ranges(ranges_string):
-    lst = (x.split("-") for x in ranges_string.split(","))
-    g = ()
-    for cell in lst:
-        g = itertools.chain(g, (num for num in range(int(cell[0]), int(cell[1]) + 1)))
-    return g
+def parse_ranges(ranges_string: str) -> Generator:
+    """
+    receives a list of ranges and return a generator containing all the number within those ranges
+    :param ranges_string: the string containing the ranges
+    :return: generator af all the numbers
+    """
+    range_list: Generator[list[str]] = (x.split("-") for x in ranges_string.split(","))
+    return (num for cell in range_list for num in range(int(cell[0]), int(cell[1]) + 1))
 
 ################################## 4.3.4 ##################################
 
 
-def get_fibo():
-    x = 0
-    y = 1
+def get_fibo() -> Generator:
+    """
+    :return: a generator containing a number from the fibonacci sequence
+    """
+    x: int = 0
+    y: int = 1
     yield x
     yield y
     while True:
@@ -49,3 +70,22 @@ def get_fibo():
         y = y + x
         x = temp
         yield y
+
+
+def main():
+    print(translate("el gato esta en la casa"))
+    print(first_prime_over(1000000))
+    print(list(parse_ranges("0-0,4-8,20-21,43-45")))
+
+    fibo_gen = get_fibo()
+    print(next(fibo_gen))
+    print(next(fibo_gen))
+    print(next(fibo_gen))
+    print(next(fibo_gen))
+    print(next(fibo_gen))
+    print(next(fibo_gen))
+    print(next(fibo_gen))
+
+
+if __name__ == '__main__':
+    main()
